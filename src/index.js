8afa539c9db22169b1c4307fca739dd80b2cd07a
Gar <gar@tut.by>
2021-03-03 17:52:40 +0300

    fix: solve reverseInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n); 
+    let str = n.toString();
+    let result = '';
+    for(i = str.length - 1; i >= 0; i--) {
+      result += str[i]; 
+    }
+    result =  Number(result);
+   return result;
 }
